708ea27119b8b7d9121547892bc4e8e032aa4803
DNL213 <dn.luu03@gmail.com>
2018-07-11 04:13:28 -0700

    Implements calling and formatting Pokemon data from API.

--- src/index.js
+++ src/index.js
@@ -8,10 +8,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 
 
-let POKEMON_LIST = [];
-for (let i = 1; i <= 151; i++) {
-    POKEMON_LIST[i] = i;
-}
+
 
 let Pokedex = require('pokeapi-js-wrapper');
 
@@ -19,43 +16,67 @@ let options = {
     protocol: 'https',
     versionPath: '/api/v2/',
     cache: true,
-    timeout: 5000 * 1000 // 5s
+    timeout: 60 * 1000 // 5s
   }
 
 let P = new Pokedex.Pokedex(options);
 
 
-let pokeArray = [];
-for (let i = 1; i <= 85; i++) {
+// pokeData represents data for all pokemon to be representated in pokedex
+let pokeData = [];
+
+
+// change for loop indexes to select which pokemon to include by ID
+// i.e. 1-151 for the original 151 pokemon
+for (let i = 188; i <= 188; i++) {
     let pokemonUrl = 'api/v2/pokemon/' + i;
 
-    let pokeData = P.resource(pokemonUrl)
+    P.resource(pokemonUrl)
     .then((response) => {
         let promise = response;
+
         // console.log(response);
-        // console.log(response.name);
-        // console.log(response.stats);
-        // console.log(response.types[0]);
-        // console.log(response.height);
-        // console.log(response.sprites.front_default);
+
+        // create object of base stats
+        let baseStats = {
+            "speed": response.stats[0].base_stat,
+            "special-defense": response.stats[1].base_stat,
+            "special-attack": response.stats[2].base_stat,
+            "defense": response.stats[3].base_stat,
+            "attack": response.stats[4].base_stat,
+            "hp": response.stats[5].base_stat
+        }
+
+        // create array of types
+        let types = [];
+        response.types.forEach((type) => {
+            types.push(type.type.name);
+        });
         
+        // create array of possible moves, maybe make this an object?
+        let moveSet = [];
+        response.moves.forEach((move) => {
+            moveSet.push(move.move.name);
+        });
+
+        // create object representing pokemon with information
         let pokemon = {
             name: response.name,
             id: i,
-            stats: response.stats,
-            type: response.types['0'].type.name,
+            stats: baseStats,
+            types: types,
             height: response.height,
+            weight: response.weight,
+            moves: moveSet,
             sprite: response.sprites.front_default
         }
-        pokeArray.push(pokemon);
+        pokeData.push(pokemon);
         
         return promise;
     })
     .catch((error) => {
         console.log(error);
     })
-
-
 
     // let pokemon = {
     //     name: pokeData.name,
@@ -67,22 +88,8 @@ for (let i = 1; i <= 85; i++) {
     // }
     // pokeArray.push(pokemon);
 }
-console.log(pokeArray);
 
+// console.log(pokeData);
 
-
-// let x = fetch("https://pokeapi.com/api/v2/pokemon/155")
-//     .then((response) => {
-//         let promise = response.json(); 
-
-//         return promise;
-//     })
-//     .catch((error) => {
-//         console.log(error);
-//     })
-
-
-
-
-ReactDOM.render(<App pokedex={POKEMON_LIST}/>, document.getElementById('root'));
+ReactDOM.render(<App pokedex={pokeData}/>, document.getElementById('root'));
 registerServiceWorker();
